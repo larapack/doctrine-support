02c9fc73324d9111bb03bb8b087653afde8972e0
Mark Topper <mark@ulties.com>
2017-09-21 18:46:37 +0200

    Overwrite MySQL Resolver

--- src/DoctrineSupportServiceProvider.php
+++ src/DoctrineSupportServiceProvider.php
@@ -4,7 +4,6 @@ namespace Larapack\DoctrineSupport;
 
 use Doctrine\DBAL\Types\Type;
 use Illuminate\Database\Connection;
-use Illuminate\Database\Schema\Grammars\MySqlGrammar;
 use Illuminate\Support\ServiceProvider;
 use Larapack\DoctrineSupport\Connections\MySqlConnection;
 use Larapack\DoctrineSupport\Types\EnumType;
@@ -27,27 +26,12 @@ class DoctrineSupportServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->registerMySqlDatabaseConnection();
-    }
-
-    /**
-     * Register MySQL database connection.
-     */
-    protected function registerMySqlDatabaseConnection()
-    {
-        $this->app->singleton('db.connection.mysql', function ($app, $parameters) {
-            // First, we list the passes parameters into single
-            // variables. I do this because it is far easier
-            // to read than using it as eg $parameters[0].
-            list($connection, $database, $prefix, $config) = $parameters;
-
-            // Next we can initialize the connection.
+        // Set resolver for MySQL
+        Connection::resolverFor('mysql', function ($connection, $database, $prefix = '', array $config = []) {
             $connection = new MySqlConnection($connection, $database, $prefix, $config);
 
             // Add Doctrine types for better support
             $this->addDoctrineTypes($connection);
-
-            $connection->setSchemaGrammar(new MySqlGrammar());
 
             return $connection;
         });
